fd04a21a155a98d56809daf13b69de8f1c45bca2
LARISSA SCHMITZ <larissa_schmitz1@estudante.sesisenai.org.br>
2025-04-24 10:50:11 -0300

    Aplicação de polimorfismo no método atacar

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
-class personagem{
-    constructor( vida, força, mana){
-        this.vida = vida,
-        this.força = força,
+
+class Personagem {
+    constructor(vida, força, mana) {
+        this.vida = vida
+        this.força = força
         this.mana = mana
     }
 }
-class Guerreiro extends personagem{
-    constructor(vida, força, mana){
+
+class Guerreiro extends Personagem {
+    constructor(vida, força, mana) {
         super(vida, força, mana)
     }
     atacar() {
-        console.log(atacando)
+        console.log("O guerreiro da um golpe com a espada")
     }
     defender() {
-        console.log(defendendo)
+        console.log("O guerreiro ergue seu escudo para defender")
     }
     usarMagia() {
-        console.log(usandoMagia)
+        console.log("O guerreiro não usa magia muito bem... ")
     }
 }
-class  Mago extends personagem{
-    constructor(vida, força, mana){
+
+class Mago extends Personagem {
+    constructor(vida, força, mana) {
         super(vida, força, mana)
     }
     atacar() {
-        console.log(atacando)
+        console.log("O mago lança uma bola de fogo mágica")
     }
     defender() {
-        console.log(defendendo)
+        console.log("O mago conjura um escudo mágico para se proteger")
     }
     usarMagia() {
-        console.log(usandoMagia)
+        console.log("O mago utiliza um feitiço para atacar")
     }
 }
-class Arqueiro extends personagem{
-    constructor(vida, força, mana){
+
+class Arqueiro extends Personagem {
+    constructor(vida, força, mana) {
         super(vida, força, mana)
     }
     atacar() {
-        console.log(atacando)
+        console.log("O arqueiro dispara uma flecha ")
     }
     defender() {
-        console.log(defendendo)
+        cosole.log("O arqueiro esquiva para evitar o ataque")
     }
     usarMagia() {
-        console.log(usandoMagia)
+        console.log("O arqueiro tenta usar uma magia com sua flecha.")
     }
 }
-const Personagem = new personagem (100, 50, 50)
+
+const guerreiro = new Guerreiro(30, 80, 20)
+const mago = new Mago(60, 40, 90)
+const arqueiro = new Arqueiro(60, 30, 40)
+
+guerreiro.atacar()
+mago.atacar()
+arqueiro.atacar()
