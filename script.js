9b94d646aea52241414dc428627c36be28cde235
LARISSA SCHMITZ <larissa_schmitz1@estudante.sesisenai.org.br>
2025-04-24 07:55:08 -0300

    sla

---
+++ script.js
@@ -0,0 +1,8 @@
+class personagem{
+    constructor( vida, força, mana){
+        this.vida = vida,
+        this.força = força,
+        this.mana = mana
+    }
+}
+const Personagem = new personagem (100, 50, 50)
