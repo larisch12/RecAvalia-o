e82f9fc386dd5d3d9d58f11f8aba055be6de1ff1
LARISSA SCHMITZ <larissa_schmitz1@estudante.sesisenai.org.br>
2025-04-24 11:01:01 -0300

    Encapsulamento de propriedades sensíveis com getters e setters

--- script.js
+++ script.js
@@ -1,54 +1,79 @@
 
 class Personagem {
+    #vida
+    #força
+    #mana
+
     constructor(vida, força, mana) {
-        this.vida = vida
-        this.força = força
-        this.mana = mana
+        this.#vida = vida
+        this.#força = força
+        this.#mana = mana
+    }
+
+    get vida() {
+        return this.#vida
+    }
+
+    set vida(valor) {
+        this.#vida = valor
+    }
+
+    get força() {
+        return this.#força
+    }
+
+    set força(valor) {
+        this.#força = valor
+    }
+
+    get mana() {
+        return this.#mana
+    }
+
+    set mana(valor) {
+        this.#mana = valor
     }
 }
 
 class Guerreiro extends Personagem {
-    constructor(vida, força, mana) {
-        super(vida, força, mana)
-    }
     atacar() {
-        console.log("O guerreiro da um golpe com a espada")
+        console.log("O guerreiro usa um golpe poderoso com a espada")
     }
+
     defender() {
-        console.log("O guerreiro ergue seu escudo para defender")
+        console.log("O guerreiro ergue seu escudo")
     }
+
     usarMagia() {
-        console.log("O guerreiro não usa magia muito bem... ")
+        console.log("O guerreiro não usa magia...")
     }
 }
 
 class Mago extends Personagem {
-    constructor(vida, força, mana) {
-        super(vida, força, mana)
-    }
     atacar() {
         console.log("O mago lança uma bola de fogo mágica")
     }
+
     defender() {
-        console.log("O mago conjura um escudo mágico para se proteger")
+        console.log("O mago conjura um escudo mágico")
     }
+
     usarMagia() {
-        console.log("O mago utiliza um feitiço para atacar")
+        console.log("O mago utiliza um feitiço")
     }
 }
 
 class Arqueiro extends Personagem {
-    constructor(vida, força, mana) {
-        super(vida, força, mana)
-    }
     atacar() {
-        console.log("O arqueiro dispara uma flecha ")
+        console.log("O arqueiro dispara uma flecha")
     }
+
     defender() {
-        cosole.log("O arqueiro esquiva para evitar o ataque")
+        console.log("O arqueiro desvia")
     }
+
     usarMagia() {
-        console.log("O arqueiro tenta usar uma magia com sua flecha.")
+        console.log("O arqueiro usa uma flecha magica")
     }
 }
 
@@ -59,3 +84,17 @@ const arqueiro = new Arqueiro(60, 30, 40)
 guerreiro.atacar()
 mago.atacar()
 arqueiro.atacar()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
